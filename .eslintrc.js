16f926880765ff4c1e927fafe9dcfc05d646ffa8
Gabriel Aragão <gabriel.aragao.comp@gmail.com>
2019-12-28 11:58:15 -0300

    Adding code style pattern

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,38 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   rules: {
-  },
+    'class-methods-use-this': ['off'],
+    'import/newline-after-import': ['off'],
+    camelcase: ['off'],
+    'no-unused-vars': ['off'],
+    'no-param-reassign': ['off'],
+    'no-named-as-default': ['off'],
+    'jsx-a11y/href-no-hash': ['off'],
+    'max-len': [
+      'warn',
+      {
+        code: 80,
+        tabWidth: 2,
+        comments: 80,
+        ignoreComments: false,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true
+      }
+    ]
+  }
 };
